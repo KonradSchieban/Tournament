e7156606cefe29eac762768647bd97c625b452d6
KonradSchieban <konrad.schieban@gmail.com>
2016-08-19 19:08:48 +0200

    Initial Draft of Schema

--- tournament.sql
+++ tournament.sql
@@ -6,3 +6,14 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP TABLE IF EXISTS players;
+CREATE TABLE players (id SERIAL PRIMARY KEY,
+                     name varchar(50) NOT NULL,
+                     wins INTEGER DEFAULT 0,
+                     matches INTEGER DEFAULT 0);
+
+DROP TABLE IF EXISTS matches;
+CREATE TABLE matches (id SERIAL PRIMARY KEY,
+                      player1 INTEGER NOT NULL,
+                      player2 INTEGER NOT NULL,
+                      winner INTEGER NOT NULL);
