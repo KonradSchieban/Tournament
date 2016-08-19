08ebb10aee41f7549d656fa929332239bb6963ac
KonradSchieban <konrad.schieban@gmail.com>
2016-08-20 00:13:38 +0200

    Views players_matches_v and players_wins_matches_v created

--- tournament.sql
+++ tournament.sql
@@ -5,14 +5,25 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
-
-DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS players CASCADE;
 CREATE TABLE players (id SERIAL PRIMARY KEY,
-                     name varchar(50) NOT NULL,
-                     wins INTEGER DEFAULT 0,
-                     matches INTEGER DEFAULT 0);
+                     name varchar(50) NOT NULL);
 
-DROP TABLE IF EXISTS matches;
+
+DROP TABLE IF EXISTS matches CASCADE;
 CREATE TABLE matches (id SERIAL PRIMARY KEY,
                       winner INTEGER NOT NULL,
                       loser INTEGER NOT NULL);
+
+CREATE VIEW players_matches_v AS
+SELECT players.id,name,COUNT(matches.id) AS num_matches
+FROM players
+LEFT JOIN matches ON players.id=matches.winner OR players.id=matches.loser
+GROUP BY players.id,name;
+
+CREATE VIEW players_wins_matches_v AS
+SELECT players_matches_v.id,name,COUNT(matches.winner) AS wins, num_matches
+FROM players_matches_v
+LEFT JOIN matches ON players_matches_v.id=matches.winner
+GROUP BY players_matches_v.id,name,matches.winner,num_matches
+ORDER BY wins DESC;
