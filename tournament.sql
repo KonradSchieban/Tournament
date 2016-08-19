f5a146f2f174fc0d81c18562e0881999017f1c1a
KonradSchieban <konrad.schieban@gmail.com>
2016-08-20 01:45:56 +0200

    players_ranked_v and swiss_pairs added

--- tournament.sql
+++ tournament.sql
@@ -27,3 +27,14 @@ FROM players_matches_v
 LEFT JOIN matches ON players_matches_v.id=matches.winner
 GROUP BY players_matches_v.id,name,matches.winner,num_matches
 ORDER BY wins DESC;
+
+CREATE VIEW players_ranked_v AS
+SELECT ROW_NUMBER() OVER (ORDER BY wins DESC) AS rank,
+    id,name,wins,num_matches
+FROM players_wins_matches_v;
+
+CREATE VIEW swiss_pairs AS
+SELECT p1.id AS p1_id, p1.name AS p1_name, p2.id AS p2_id, p2.name AS p2_name
+FROM players_ranked_v p1
+JOIN players_ranked_v p2
+ON p1.rank+1=p2.rank AND p1.rank%2=1;
