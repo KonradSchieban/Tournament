0395f50557104b554d140df8d3e3d2135add4467
KonradSchieban <konrad.schieban@gmail.com>
2016-08-20 13:39:42 +0200

    Comments added

--- tournament.sql
+++ tournament.sql
@@ -1,10 +1,5 @@
 -- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+
 DROP TABLE IF EXISTS players CASCADE;
 CREATE TABLE players (id SERIAL PRIMARY KEY,
                      name varchar(50) NOT NULL);
@@ -15,12 +10,15 @@ CREATE TABLE matches (id SERIAL PRIMARY KEY,
                       winner INTEGER NOT NULL,
                       loser INTEGER NOT NULL);
 
+-- View which returns player id and name together with the number of matches he played
 CREATE VIEW players_matches_v AS
 SELECT players.id,name,COUNT(matches.id) AS num_matches
 FROM players
 LEFT JOIN matches ON players.id=matches.winner OR players.id=matches.loser
 GROUP BY players.id,name;
 
+-- View which return player id and name together with his number of wins and matches he played
+-- This view is used to create player ranking, ordered by the number of wins
 CREATE VIEW players_wins_matches_v AS
 SELECT players_matches_v.id,name,COUNT(matches.winner) AS wins, num_matches
 FROM players_matches_v
@@ -28,11 +26,14 @@ LEFT JOIN matches ON players_matches_v.id=matches.winner
 GROUP BY players_matches_v.id,name,matches.winner,num_matches
 ORDER BY wins DESC;
 
+-- View which adds the ranking number to th view players_wins_matches_v
 CREATE VIEW players_ranked_v AS
 SELECT ROW_NUMBER() OVER (ORDER BY wins DESC) AS rank,
     id,name,wins,num_matches
 FROM players_wins_matches_v;
 
+-- View which returns all swiss pairs according to the tables "players" and "matches"
+-- It is created by self joining the player ranking view "players_ranked_v".
 CREATE VIEW swiss_pairs AS
 SELECT p1.id AS p1_id, p1.name AS p1_name, p2.id AS p2_id, p2.name AS p2_name
 FROM players_ranked_v p1
